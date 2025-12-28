78ede4120adc9bbf43e200c5e0f6ff454988003c
Egor <e.busygin2003@gmail.com>
2025-12-28 11:21:37 +0300

    feature (DB) initiate database - create prisma db - create models - create db service

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import { LogService } from './common/log-service/log.service';
 import { ILogService } from './common/log-service/log.service.interface';
 import { IEnvService } from './common/env-service/env.service.interface';
 import { EnvService } from './common/env-service/env.service';
+import { IDatabaseService } from './common/database-service/database-service.interface';
+import { DatabaseService } from './common/database-service/database-service';
 
 const appBindings = new ContainerModule((options: ContainerModuleLoadOptions) => {
   options.bind<App>(DI_TYPES.App).to(App).inSingletonScope();
   options.bind<ILogService>(DI_TYPES.LogService).to(LogService).inSingletonScope();
   options.bind<IEnvService>(DI_TYPES.EnvService).to(EnvService).inSingletonScope();
+  options.bind<IDatabaseService>(DI_TYPES.DatabaseService).to(DatabaseService).inSingletonScope();
 });
 
 async function boot(): Promise<void> {
