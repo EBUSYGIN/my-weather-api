938550e990fead166ade0a2951eedafd25dacd2f
Egor <e.busygin2003@gmail.com>
2026-01-02 18:39:48 +0300

    feat(api) add correct response codes -add enum to handle http response codes

--- src/common/middlewares/user/user.middleware.ts
+++ src/common/middlewares/user/user.middleware.ts
@@ -2,20 +2,21 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
 import { IMiddleware } from '../middleware.interface';
+import { HttpResponses } from '../../types/http-responses.types';
 
 export class UserMiddleware implements IMiddleware {
   constructor(private classToValidate: ClassConstructor<object>) {}
 
   execute = ({ body }: Request, res: Response, next: NextFunction): void => {
     if (!body) {
-      res.status(422).send({ message: 'Неправильное тело запроса' });
+      res.status(HttpResponses.UNPROCESSABLE_ENTITY).send({ message: 'Неправильное тело запроса' });
       return;
     }
 
     const instance = plainToInstance(this.classToValidate, body);
     validate(instance).then((errors) => {
       if (errors.length) {
-        res.status(422).send(errors);
+        res.status(HttpResponses.UNPROCESSABLE_ENTITY).send(errors);
       } else {
         next();
       }
